159ac4b9f7de24e90c8f033976297466277bc3cf
srinivas <srinivas@srinivass-mbp.lan>
2020-03-21 19:29:58 -0400

    commit made

---
+++ hello/src/hello/test/Test.java
@@ -0,0 +1,26 @@
+package hello.test;
+
+public class Test {
+	int[] pos;
+	int[] neg;
+	int[] zer;
+	
+	static void sum(int[] arr)
+	{
+		
+		int count=arr.length;
+		for(int i=0;i<count;i++)
+		{
+			if(arr[i]<0) {
+				
+				
+				
+			}
+		}
+		
+		
+		
+		
+		
+	}
+}
